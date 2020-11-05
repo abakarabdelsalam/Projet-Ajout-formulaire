045f0265fcfe180569f8615eabe36c405c0a7ff3
abakarabdelsalam <abdelsalamabakar3@gmail.com>
2020-11-05 09:32:48 +0100

    variable local(reference)

--- AjoutFormulaire/src/app/app.component.ts
+++ AjoutFormulaire/src/app/app.component.ts
@@ -7,11 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public fruit: string;
-  public fruits: string[] = [];
+  public fruits: string[] = ['pomme','tomat', 'grenadine'];
   
 
   SupprimeFruit(fruit:string) {
     this.fruits.splice(this.fruits.indexOf(fruit), 1);
+    
+  }
+
+  FonctionRef(reference: any) {
+    console.log(reference.type);
+    console.log(reference.value);
+    this.fruits.push(reference.value);
+
+
     
   }
 }
